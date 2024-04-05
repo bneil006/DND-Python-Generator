a2ecb5ac9abc43a28e576976f0bf972258ff58b6
bneil006 <brmneil2@gmail.com>
2024-04-04 22:11:21 -0500

    created a base stats function to increase based on classes saving throws so that we have a better baseline when creating a base stat scheme for npcs

--- test.py
+++ test.py
@@ -32,45 +32,24 @@ class Random_Npc():
         Random_Npc._id_counter += 1
         self.id = Random_Npc._id_counter
         self.name = gen.generate_full_name(gen.first_name_elements, gen.last_name_elements)
-        self.race, self.subrace = Random_Npc.choose_race(self)
+        self.npc_race, self.npc_subrace = Random_Npc.choose_race(self)
         self.npc_class = Random_Npc.choose_class(self)
-        print(f"ID: {self.id}, Name: {self.name}, Race: {self.race["name"]}, Subrace: {self.subrace["name"]}, Class: {self.npc_class["name"]}")
+        self.npc_class_proficiencies = self.npc_class["proficiencies"]
+        self.base_stats = self.set_base_stats()
 
-class Race:
-    BASE_STATS = {"STR": 8, "DEX": 8, "CON": 8, "INT": 8, "WIS": 8, "CHA": 8}
-
-    def __init__(self, race_info, subrace_info=None):
-        self.race_info = race_info
-        self.subrace_info = subrace_info
-        self.stats = Race.BASE_STATS.copy()
-        self.bonus_stats = {"details": []}
-        self.base_stat_modifiers()
-
-    def class_race_info(self):
-        return f"Race: {self.race_info['name']}, Subrace: {self.subrace_info['name'] if self.subrace_info else 'None'}"
-
-    def base_stat_modifiers(self):
-        point_buy = 27
-        cost_table = {8: 0, 9: 1, 10: 2, 11: 3, 12: 4, 13: 5, 14: 7, 15: 9}
+        print(f"""Name: {self.name}, Race: {self.npc_race["name"]}, Subrace: {self.npc_subrace["name"]}, 
+Saving Throws: {self.npc_class_proficiencies["saving_throws"]}
+Base Stats: {self.base_stats}""")
         
-        while point_buy > 0:
-            stats_keys = list(self.stats.keys())
-            random.shuffle(stats_keys)
-            for stat_modifier in stats_keys:
-                stat_level = self.stats[stat_modifier]
-                if stat_level < 15:
-                    next_cost = cost_table.get(stat_level + 1, float('inf')) - cost_table.get(stat_level, 0)
-                    if point_buy >= next_cost:
-                        self.stats[stat_modifier] += 1
-                        point_buy -= next_cost
-                        break
-
-    def highest_modifier(self):
-        highest_stat = max(self.stats, key=self.stats.get)
-        return highest_stat, self.stats[highest_stat]
+    def set_base_stats(self):
+            base_stats = {"STR": 8, "DEX": 8, "CON": 8, "INT": 8, "WIS": 8, "CHA": 8}
+            for stat in base_stats:
+                  if stat in self.npc_class_proficiencies["saving_throws"]:
+                        base_stats[stat] += 2
+            return base_stats
 
 def create_random_npc(num):
      for i in range(num):
           Random_Npc()
 
-create_random_npc(5)
+create_random_npc(1)
