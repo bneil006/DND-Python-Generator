c43e479d4cccfb98d3cffe50d446b4f9b167c508
bneil006 <brmneil2@gmail.com>
2024-04-08 16:10:35 -0500

    added a way to add additional fields such as jobs to the npcs generated by the town selection

--- town.py
+++ town.py
@@ -5,6 +5,7 @@ class Area():
     def __init__(self):
         self.set_population()
         self.generate_npcs()
+        self.set_npc_jobs()
 
     def set_population(self):
         self.population = random.randint(0, 5)
@@ -12,34 +13,37 @@ class Area():
     def generate_npcs(self):
         self.npcs = create_town_dump(self.population)
 
-    def get_demographic_information(self):
-        job_list = ["Retired", "Unemployeed", "Labourer", "Merchant", "Bartender", "Cook", "Blacksmith"]
-        demographic_info = {"npcs": []}
+    def generate_buildings(self):
+        # 1% should be noble
+        # 5% of the population should be guards
+        # 7% should be unemployeed
+        # 8% should be retired
+        # 9% should be business owners
+        # 10% should be adventurers
+        # 10% should be mercenaries
+        # 50% should be workers
 
+        noble_npcs, guard_npcs, unemployeed_npcs, retired_npcs = 0, 0, 0, 0
+        business_owner_npcs, adventurer_npcs, mercenary_npcs, worker_npcs = 0, 0, 0, 0
+        pass
+
+    def set_npc_jobs(self):
+        updated_npcs = []
         for npc in self.npcs["npcs"]:
-            demo_details = {
-                "name": npc["name"],
-                "race": npc["race"],
-                "subrace": npc["subrace"],
-                "job": random.choice(list(job_list))
+            npc_addition = {
+                "job": "Blacksmith"
             }
-            demographic_info["npcs"].append(demo_details)
-        print(demographic_info)
+            updated_npcs.append({**npc, **npc_addition})
+        self.npcs["npcs"] = updated_npcs
+
+
+            
 
 class Hamlet(Area):
     def __init__(self):
         super().__init__()
 
     def set_population(self):
-        self.population = random.randint(80, 120)
-    
-class SmallTown(Area):
-    def __init__(self):
-        super().__init__()
+        self.population = random.randint(1, 3)
 
-    def set_population(self):
-        self.population = random.randint(2000, 2500)
-
-
-x = Hamlet()
-x.get_demographic_information()
+print(Hamlet().npcs)
